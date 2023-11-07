d5e1a233394ea2ecf74ce4168ec20975f3d36d90
Paulo Augusto <82547096+pauloaugustojf@users.noreply.github.com>
2023-11-07 08:55:39 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 ZC.LICENSE = ["569d52cefae586f634c54f86dc99e6a9", "b55b025e438fa8a98e32482b5f768ff5"];
     window.feed = function(callback) {
       var tick = {};
-      tick.plot0 = '450'; /*Math.ceil(350 + (Math.random() * 500));
-      callback(JSON.stringify(tick));*/
+      tick.plot0 = '450'; /*Math.ceil(350 + (Math.random() * 500)); */
+      callback(JSON.stringify(tick));
     };
  
     var myConfig = {
